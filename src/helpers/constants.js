9a963adbc0284ed0b772a86bd3eaf4987d27e505
Hardik <hardik11.chauhan@gmail.com>
2019-07-14 23:11:20 +0530

    Changing server url

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -2,7 +2,7 @@
 //local url : http://192.168.100.5:8000/
 import {smileO, frownO, mehO} from 'react-icons-kit/fa';
 
-export const SERVER_URL = "http://192.168.100.5:8000/";
+export const SERVER_URL = "https://baroque-choucroute-81714.herokuapp.com/";
 export const EMOJI = {
     neutral: {
         icon:mehO,
